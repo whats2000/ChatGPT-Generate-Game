9cf146a483f093c26d49d640427b611e5a7e15a9
whats2000 <whats2000mc@gmail.com>
2023-10-08 11:15:19 +0800

    Add sound setting

--- SpaceShipGameOptimization/GameComponent/Game/GameSound.js
+++ SpaceShipGameOptimization/GameComponent/Game/GameSound.js
@@ -15,8 +15,8 @@ class GameSound extends Howl {
     static BackgroundMusic = new GameSound({
         src: ['static/sound/BackgroundMusic.mp3'],
         loop: true,
-        volume: 0.8,
-        playerSetVolume: 0.8
+        volume: 0.5,
+        playerSetVolume: 0.5
     });
 
     /**
@@ -27,7 +27,7 @@ class GameSound extends Howl {
         src: ['static/sound/BossBackgroundMusic.mp3'],
         loop: true,
         volume: 0,
-        playerSetVolume: 0.8
+        playerSetVolume: 0.5
     });
 
     /**
@@ -74,8 +74,8 @@ class GameSound extends Howl {
         }),
         "C": new GameSound({
             src: ['static/sound/PlayerCrash.mp3'],
-            volume: 1,
-            playerSetVolume: 1
+            volume: 0.2,
+            playerSetVolume: 0.2
         })
     };
 
@@ -83,18 +83,33 @@ class GameSound extends Howl {
      * Create a Howl instance for the shield active sound.
      * @type {GameSound}
      */
-    static shieldActiveSound = new GameSound({
+    static ShieldActiveSound = new GameSound({
         src: ['static/sound/ShieldActive.mp3'],
         volume: 1,
         playerSetVolume: 1
     });
 
+    /**
+     * Set up volume controls for different sound categories.
+     * This method binds input elements to control the volume for various sounds.
+     * @static
+     */
     static setUpVolumeControl() {
-        // Get the volume slider element by its ID
-        const backgroundMusicVolumeSlider = document.getElementById('background-music-slider');
+        // Bind the volume name to control for specific sounds
+        GameSound.BackgroundMusic.#bindVolumeControl('background-music');
+        GameSound.BossBackgroundMusic.#bindVolumeControl('boss-music');
+        GameSound.LaunchSound['A'].#bindVolumeControl('player-shoot');
+        GameSound.LaunchSound['B'].#bindVolumeControl('enemy-shoot');
+        GameSound.ShieldActiveSound.#bindVolumeControl('others');
+        GameSound.WarningSound.#bindVolumeControl('others');
 
-        // Bind the volume slider to control the master volume
-        GameSound.BackgroundMusic.#bindVolumeControl(backgroundMusicVolumeSlider);
+        // Iterate through the ExplosionSound object and bind volume controls
+        for (const soundName in GameSound.ExplosionSound) {
+            if (GameSound.ExplosionSound.hasOwnProperty(soundName)) {
+                // Bind volume control for each explosion sound
+                GameSound.ExplosionSound[soundName].#bindVolumeControl('explosion');
+            }
+        }
     }
 
     /**
@@ -194,16 +209,39 @@ class GameSound extends Howl {
     }
 
     /**
-     * Bind an input element to control the volume.
-     * @param {HTMLInputElement} inputElement - The input element (range slider) to bind.
+     * Bind an input element and mute button to control the volume.
+     * @param {string} volumeName - The volume name to bind.
      * @private
      */
-    #bindVolumeControl(inputElement) {
+    #bindVolumeControl(volumeName) {
         // Add an event listener to the input element to update the volume when it changes
+        const inputElement = document.getElementById(`${volumeName}-slider`);
         inputElement.addEventListener('input', (event) => {
-            if (this.getVolume() === this.playerSetVolume)
+            if (this.getVolume() === this.playerSetVolume) {
                 this.setVolume(parseFloat(event.target.value));
-            this.playerSetVolume = parseFloat(event.target.value);
+                this.playerSetVolume = parseFloat(event.target.value);
+            }
+        });
+
+        // Add an event listener to the mute button to toggle mute and change the icon
+        const muteButton = document.getElementById(`${volumeName}-mute-button`);
+        muteButton.addEventListener('click', () => {
+            if (!this.mute()) {
+                // Volume is not muted, mute it
+                this.mute(true);
+                muteButton.querySelector('.volume-icon').textContent = '🔇'; // Change the icon to muted speaker
+            } else {
+                // Volume is muted, unmute it to the last set volume
+                this.mute(false);
+                muteButton.querySelector('.volume-icon').textContent = '🔊'; // Change the icon back to speaker
+            }
+        });
+
+        // Add an event listener to the mute button to prevent Enter key behavior
+        muteButton.addEventListener('keydown', (event) => {
+            if (event.key === 'Enter') {
+                event.preventDefault(); // Prevent the default Enter key behavior
+            }
         });
     }
 }
